2b1d743818eeaa1022b99a52e4bb6cba3026bc58
Srivatsan Ramanujam <sramanujam@gopivotal.com>
2014-09-30 16:34:33 -0400

    gppkgs get generated now, though hawq installer still has only GPDB modules - will fix next

--- src/ports/greenplum/cmake/GreenplumUtils.cmake
+++ src/ports/greenplum/cmake/GreenplumUtils.cmake
@@ -13,57 +13,51 @@ function(define_greenplum_features IN_VERSION OUT_FEATURES)
     set(${OUT_FEATURES} "${${OUT_FEATURES}}" PARENT_SCOPE)
 endfunction(define_greenplum_features)
 
-function(add_gppkg GPDB_VERSION GPDB_VARIANT GPDB_VARIANT_SHORT UPGRADE_SUPPORT)
-    string(REPLACE "." "_" VERSION_ "${GPDB_VERSION}")
-
-    file(WRITE "${CMAKE_BINARY_DIR}/deploy/gppkg/${GPDB_VARIANT}_${VERSION_}_gppkg.cmake" "
+function(add_gppkg GPDB_VERSION GPDB_VARIANT GPDB_VARIANT_SHORT)
+    file(WRITE "${CMAKE_BINARY_DIR}/deploy/gppkg/Version_${IN_PORT_VERSION}.cmake" "
     file(MAKE_DIRECTORY
-        \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}/BUILD\"
-        \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}/SPECS\"
-        \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}/RPMS\"
-        \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}/gppkg\"
+        \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}/BUILD\"
+        \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}/SPECS\"
+        \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}/RPMS\"
+        \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}/gppkg\"
     )
-
     set(GPDB_VERSION \"${GPDB_VERSION}\")
     set(GPDB_VARIANT \"${GPDB_VARIANT}\")
     set(GPDB_VARIANT_SHORT \"${GPDB_VARIANT_SHORT}\")
-    set(UPGRADE_SUPPORT \"${UPGRADE_SUPPORT}\")
-    string(TOLOWER \"${GPDB_VARIANT}\" PORT_NAME)
 
     configure_file(
         pdltools.spec.in
-        \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}/SPECS/pdltools.spec\"
+        \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}/SPECS/pdltools.spec\"
     )
     configure_file(
         gppkg_spec.yml.in
-        \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}/gppkg/gppkg_spec.yml\"
+        \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}/gppkg/gppkg_spec.yml\"
     )
-
     if(GPPKG_BINARY AND RPMBUILD_BINARY)
-        add_custom_target(gppkg_${GPDB_VARIANT}_${VERSION_}
+        add_custom_target(gppkg_${PORT_VERSION_UNDERSCORE}
             COMMAND cmake -E create_symlink \"\${PDLTOOLS_GPPKG_RPM_SOURCE_DIR}\"
                 \"\${CPACK_PACKAGE_FILE_NAME}-gppkg\"
             COMMAND \"\${RPMBUILD_BINARY}\" -bb SPECS/pdltools.spec
-            COMMAND cmake -E rename \"RPMS/\${PDLTOOLS_GPPKG_RPM_FILE_NAME}\"
-                \"gppkg/\${PDLTOOLS_GPPKG_RPM_FILE_NAME}\"
+            COMMAND cmake -E rename "RPMS/\${PDLTOOLS_GPPKG_RPM_FILE_NAME}"
+                "gppkg/\${PDLTOOLS_GPPKG_RPM_FILE_NAME}"
             COMMAND \"\${GPPKG_BINARY}\" --build gppkg
             DEPENDS \"${CMAKE_BINARY_DIR}/\${CPACK_PACKAGE_FILE_NAME}.rpm\"
-            WORKING_DIRECTORY \"\${CMAKE_CURRENT_BINARY_DIR}/${GPDB_VERSION}\"
-            COMMENT \"Generating ${GPDB_VARIANT} ${GPDB_VERSION} gppkg installer...\"
+            WORKING_DIRECTORY \"\${CMAKE_CURRENT_BINARY_DIR}/${IN_PORT_VERSION}\"
+            COMMENT \"Generating Greenplum ${IN_PORT_VERSION} gppkg installer...\"
             VERBATIM
         )
     else(GPPKG_BINARY AND RPMBUILD_BINARY)
-        add_custom_target(gppkg_${GPDB_VARIANT}_${VERSION_}
+        add_custom_target(gppkg_${PORT_VERSION_UNDERSCORE}
             COMMAND cmake -E echo \"Could not find gppkg and/or rpmbuild.\"
                 \"Please rerun cmake.\"
         )
     endif(GPPKG_BINARY AND RPMBUILD_BINARY)
-
+    
     # Unfortunately, we cannot set a dependency to the built-in package target,
     # i.e., the following does not work:
     # add_dependencies(gppkg package)
-
-    add_dependencies(gppkg gppkg_${GPDB_VARIANT}_${VERSION_})
+    
+    add_dependencies(gppkg gppkg_${PORT_VERSION_UNDERSCORE})
     ")
 endfunction(add_gppkg)
 
