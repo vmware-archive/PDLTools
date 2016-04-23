d8c1df45dc9cf0fab30a9a669b11fc0c5e4c51ae
= <=>
2016-04-23 16:40:51 -0400

    change GRANT ALL to GRANT SELECT

--- src/pdlpack/pdlpack.py
+++ src/pdlpack/pdlpack.py
@@ -1295,7 +1295,7 @@ def install_check(schema, platform, sugar_schema, madlib_schema, args):
 	    session.exec_query("DROP USER IF EXISTS %s;" % (test_user), True)
 	session.exec_query("CREATE USER %s;" % (test_user), True)
 	session.exec_query("GRANT ALL ON SCHEMA %s TO %s;" %(schema, test_user), True)
-	session.exec_query("GRANT ALL ON SCHEMA %s TO %s;" %(madlib_schema, test_user), True)
+	session.exec_query("GRANT SELECT ON SCHEMA %s TO %s;" %(madlib_schema, test_user), True)
 	session.exec_query("GRANT ALL ON SCHEMA %s TO %s;" %(sugar_schema, test_user), True)
 
 	# 2) Run test SQLs
